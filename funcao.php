dea3ea18cd58d5cc07190c08a14c66ed268f5461
unknown <Everton.gs.br1@gmail.com>
2023-07-28 02:00:11 -0300

    Calculadora

--- funcao.php
+++ funcao.php
@@ -1,2 +1,16 @@
 <?php
 
+function soma($x,$y)
+{
+    return $x+$y;
+}
+
+function divisao($x,$y)
+{
+    return $x/$y;
+}
+
+function mutiplicacao($x,$y)
+{
+    return $x*$y;
+}
--- index.php
+++ index.php
@@ -3,3 +3,9 @@
 require_once 'configuracao.php';
 include 'funcao.php';
 
+echo soma(5,7);
+echo "<br>";
+echo divisao(10,2);
+echo "<br>";
+echo mutiplicacao(10,2);
+echo "<br>";
